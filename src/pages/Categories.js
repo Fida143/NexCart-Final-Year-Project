a1bd1ba4349aea141138b56608d4308db0f39890
FIDA HUSSAIN <722003hussainfida@gmail.com>
2024-07-13 18:40:43 +0530

    final commit

--- src/pages/Categories.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../Layouts/Layout";
-
-const Categories = () => {
-  return <Layout title={"Categories - Ecommerce App "}>Categories</Layout>;
-};
-
-export default Categories;
